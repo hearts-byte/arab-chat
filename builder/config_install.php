ba3516e2a33baf94524726451c3c375545257536
hearts-byte <tlaqywtalt@gmail.com>
2026-08-08 06:16:53 +0300

    Delete builder directory

--- builder/config_install.php
+++
@@ -1,6 +0,0 @@
-<?php
-require(__DIR__ . '/../system/database.php');
-require(__DIR__ . '/../system/controller.php');
-require(__DIR__ . '/../system/function.php');
-require(__DIR__ . '/../system/function_all.php');
-?>
